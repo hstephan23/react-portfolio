f16dba1b839bf34f3749d22cf8e2d93252ec4a08
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2024-03-01 20:17:53 -0700

    added the contact page layout, need to add logic

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,5 +1,22 @@
 export default function Contact() {
   
+
+  return (
+    <section>
+      <form>
+        <label for="name">Name:</label><br/>
+        <input type="text" id="name" placeholder="Name"></input>
+        <br/>
+        <label for="email">Email:</label><br/>
+        <input type="email" id="email" placeholder="Email"></input>
+        <br/>
+        <label for="message">Message:</label><br/>
+        <input type="text" id="message" placeholder="Message"></input>
+        <br/>
+        <input type="submit" value="Submit"/>
+      </form>
+    </section>
+  )
 };
 
 
